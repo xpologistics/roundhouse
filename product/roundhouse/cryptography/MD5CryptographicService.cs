9777ce6b38b129f8b65299f0f7ffaa328b1d0739
Ryan Smulan <ryan.smulan@gmail.com>
2016-11-04 02:52:16 -0400

    Fixing hash issue with line endings

--- product/roundhouse/cryptography/MD5CryptographicService.cs
+++ product/roundhouse/cryptography/MD5CryptographicService.cs
@@ -15,7 +15,20 @@ namespace roundhouse.cryptography
 
         public string hash(string clear_text_of_what_to_hash)
         {
-            byte[] clear_text_bytes = Encoding.UTF8.GetBytes(clear_text_of_what_to_hash);
+            // roundhouse version returned hash based on source line endings
+            // custom returns hash based on all line endings being unix line endings
+
+            // start of custom addition
+            const string line_ending_windows = "\r\n";
+            const string line_ending_unix = "\n";
+            const string line_ending_mac = "\r";
+            string normalized_text_of_what_to_hash = clear_text_of_what_to_hash
+                .Replace(line_ending_windows, line_ending_unix).Replace(line_ending_mac, line_ending_unix);
+            // end of custom addition
+
+            // byte[] clear_text_bytes = Encoding.UTF8.GetBytes(clear_text_of_what_to_hash); // removed in custom code
+            byte[] clear_text_bytes = Encoding.UTF8.GetBytes(normalized_text_of_what_to_hash); // replacement in custom code
+
             byte[] cypher_bytes = crypto_provider.ComputeHash(clear_text_bytes);
             return Convert.ToBase64String(cypher_bytes);
         }
